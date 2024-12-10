a59893949b9192d9233875563059197b46bdb1c3
lammeckk <lameckk7@gmail.com>
2024-12-10 20:43:44 +0300

    assignment1 changes

---
+++ src/assignment/InsufficientFundsException.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class InsufficientFundsException extends Exception {
+
+    private double amount;
+    private double balance;
+
+    public InsufficientFundsException(double amount, double balance) {
+        super("Insufficient funds: Required " + amount + ", but only " + balance + " available");
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
